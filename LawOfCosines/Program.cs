3e0a2886c06bd5815ea19857dac0705a063e72cb
Andrew Palmer <andrewpalmer@microsoft.com>
2025-08-05 19:25:33 -0400

    Cleanup commit

--- LawOfCosines/Program.cs
+++ LawOfCosines/Program.cs
@@ -1,10 +1,13 @@
 ﻿using SimpleMathExamples;
+using System.ComponentModel;
 using System.Net.NetworkInformation;
 
 namespace SimpleMathExamples
 {
     public class Program
     {
+        int[] numbers = new int[] { 1, 2, 3, 4, 5 };
+
 
         static void Main(string[] args)
         {
@@ -13,6 +16,22 @@ namespace SimpleMathExamples
 
             double a = 8;
             double b = 11;
+
+            double a1 = 1;
+            double b1 = 2;
+
+            List<double> list = new List<double>();
+            List<string> listOfStrings = new List<string>();
+
+
+            list.Add(1);
+            list.Add(2);
+
+            list.Remove(1);
+
+            int[] numbers = new int[] { 1, 2, 3, 4, 5 };
+
+
 
             pythagoreanTheorem.Solve(a, b);
             // Pythagorean Theorem complete
@@ -25,7 +44,28 @@ namespace SimpleMathExamples
 
             lawOfCosines.Solve(a, b, angleC);
             // Law of Cosines complete
+
+
         }
 
+        public static int DataStructureList()
+        {
+            // Size does change
+            List<int> numbers = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+            //numbers.Count();
+
+            numbers.Add(11);
+
+            return numbers.Sum();
+        }
+
+        public static int DataStructureArray()
+        {
+            // Size doesn't change
+            int[] numbers = new int[] { 1, 2, 3, 4, 5 };
+
+            return numbers.Sum();
+        }
     }
 }
